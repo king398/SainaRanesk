9313ec217a0e144117b36b956e9dff2466f15cb5
king398 <mithilsalunkhe@outlook.com>
2022-12-27 19:12:21 +0530

    add gen all pred

---
+++ gen_all_preds.py
@@ -0,0 +1,35 @@
+from model import *
+import glob
+from tqdm import tqdm
+model = whisper.load_model("large")
+files = glob.glpath = "data/test/audio"
+ob(f"{path}/*.mp3") + glob.glob(f"{path}/*/*.mp3")
+
+
+def format_timestamp(seconds: float, always_include_hours: bool = False, decimal_marker: str = '.'):
+    assert seconds >= 0, "non-negative timestamp expected"
+    milliseconds = round(seconds * 1000.0)
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+    hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}{decimal_marker}{milliseconds:03d}"
+
+
+def transcript_timestamp(transcript: Iterator[dict], text_file):
+    for segment in transcript:
+        text_file.write(
+            f"{format_timestamp(segment['start'])} --> {format_timestamp(segment['end'])} "
+            f"Text: {segment['text'].strip().replace('-->', '->')}\n",
+        )
+    text_file.close()
+
+
+for file in files:
+    result = model.transcribe(file, task='translate', verbose=True, no_speech_threshold=0.4,
+                              condition_on_previous_text=False)
+    text_file = open(f"transcripts/{file.split('/')[-1].split('.')[0]}.txt", "w")
+    transcript_timestamp(result, text_file)
