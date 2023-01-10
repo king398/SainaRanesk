0c66ab9118a5f81c2867aed6581571dce4cac885
king398 <mithilsalunkhe@outlook.com>
2023-01-10 16:47:12 +0530

    update html

--- request.py
+++ request.py
@@ -4,10 +4,6 @@ import multiprocessing
 
 files = glob.glob("/home/mithil/PycharmProjects/SainaRanesk/data/AIML Datastes SR2.0/*.mp3")[:20]
 
-r = requests.post('http://192.168.0.126:5000/transcribe', files={
-    'file': open('/home/mithil/220714_2148.mp3', 'rb')})
-print(r.json())
-
 
 def request(file):
     r = requests.post('http://192.168.0.126:5000/transcribe', files={
@@ -15,5 +11,5 @@ def request(file):
     print(r.json())
 
 
-with multiprocessing.Pool(5) as p:
+with multiprocessing.Pool(20) as p:
     p.map(request, files)
